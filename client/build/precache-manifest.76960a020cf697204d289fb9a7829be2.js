6b13f49c71db4c93c954875bab401c60e81c8f07
PadmanabhanSajesh <v-sapv@microsoft.com>
2020-10-04 00:30:15 +0530

    initial commit

---
+++ client/build/precache-manifest.76960a020cf697204d289fb9a7829be2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c8ae088ea5c84338d75c44f8a94d29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0335b9a82ed844adef4a",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "781d6942557cb01591a6",
+    "url": "/static/js/2.2a531cdc.chunk.js"
+  },
+  {
+    "revision": "0335b9a82ed844adef4a",
+    "url": "/static/js/main.4b27b30d.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
